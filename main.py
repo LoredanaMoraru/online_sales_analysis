e5b78a6ba678f73ed1ebca337ed9c88ef95acad4
Loredana <loredanamoraru78@gmail.com>
2025-06-27 07:21:35 +0300

    Etapa 7: Modificat produse și eliminat afișarea inventarului

--- main.py
+++ main.py
@@ -5,9 +5,9 @@ from product_manager import ProductManager
 manager = ProductManager()
 
 # Adăugăm produse fictive
-p1 = Product("Laptop", 3200, 5)
-p2 = Product("Telefon", 2500, 10)
-p3 = Product("Casti", 300, 15)
+p1 = Product("Televizor", 2200, 2)
+p2 = Product("Tabletă", 1400, 7)
+p3 = Product("Imprimantă", 630, 3)
 
 manager.add_product(p1)
 manager.add_product(p2)
@@ -15,6 +15,3 @@ manager.add_product(p3)
 
 # Afișăm inventarul
 manager.display_all_products()
-
-# Afișăm valoarea totală
-manager.total_inventory_value()
